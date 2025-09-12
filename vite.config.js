7e72bef35af1d92eeebbb2c288c18a88769ad6a9
eskim <etrilles@my.centennialcollege.ca>
2025-09-11 20:39:41 -0400

    Fix Render Deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    allowedHosts: [
+      'comp229-f25-402-week2.onrender.com'
+    ]
 })
